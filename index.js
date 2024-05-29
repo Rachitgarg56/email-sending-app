9373b186c618ee699400d2447033bc6909efcb50
Rachit Garg <rachitgarg56@gmail.com>
2024-05-29 15:22:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express(); 
+
+app.use(express.urlencoded( { extended: true } ));
+
+app.post('/send-email', (req,res) => {
+    const { to, subject, message } = req.body;
+
+    // const transporter = nodemailer.createTransport({
+    //     host: "smtp.gmail.com",
+    //     port: 587,
+    //     secure: false, // Use `true` for port 465, `false` for all other ports
+    //     auth: {
+    //       user: "education1325@gmail.com",
+    //       pass: process.env.PASSWORD,
+    //     },
+    // });
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'monserrate.jacobson@ethereal.email',
+            pass: 'bw4R6WxxvxQektxzwW'
+        }
+    });
+
+    let mailOptions = {
+        from: 'education1325@gmail.com', // Sender address
+        to: to, // List of receivers
+        subject: subject, // Subject line
+        text: message // Plain text body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send('Error: ' + error.message);
+        }
+        res.send('Email sent: ' + info.response);
+    });
+    
+})
+
+app.listen(8080, () => {
+    console.log(`Server is up and running on PORT 8080`);
+})
